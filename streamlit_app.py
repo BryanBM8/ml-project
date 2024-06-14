e4212c28a43394987801b14320d53c6d212cb716
Jasson9 <52589181+Jasson9@users.noreply.github.com>
2024-06-14 18:32:21 +0000

    add auto update on random, plotting and bugfix

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,8 @@ from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder, LabelEncoder
 from sklearn.pipeline import Pipeline
 import joblib
+import matplotlib.pyplot as plt
+
 """
 # ML Project - Student Performance score prediction
 
@@ -21,17 +23,36 @@ Anggota:
 
 Repository: [Github](https://github.com/Jasson9/ml-project)
 """
-model_option = st.sidebar.selectbox("Model:", {"Random Forest":"RandomForest.pkl", "SVM":"SVM.pkl", "Gradient Boosting":"XGB.pkl"})
+st.sidebar.header("Processing Options")
+model_option = st.sidebar.selectbox("Model:", {"Random Forest":"RandomForest.pkl", "SVM":"SVM.pkl", "Gradient Boosting":"gradient_boosting.pkl"})
 scaler_option = st.sidebar.selectbox("Scaler Option:", {"Standard Scaler":"standard.pkl", "Robust Scaler":"robust.pkl", "MinMax Scaler":"minmax.pkl"})
-    
 
-def random_button_callback():
+
+def random_button_callback(set_value_only=False):
+    state = st.session_state;
     input_df =pd.read_csv('student-mat.csv', sep=';', usecols=['sex', 'age', 'address', 'Medu', 'Fedu', 
      'traveltime', 'failures', 'paid', 'higher', 'internet','goout', 'G1', 'G2'])
-    predict(input_df.sample(),model_options[model_option], scaler_options[scaler_option])
+    sample = input_df.sample()
+    state = st.session_state;
+    state.age = sample['age'].values[0]
+    state.sex_idx = list(sex_options.values()).index(sample['sex'].values[0])
+    state.address_idx = list(address_options.values()).index(sample['address'].values[0])
+    state.Medu_idx = list(Medu_options.values()).index(sample['Medu'].values[0])
+    state.Fedu_idx = list(Fedu_options.values()).index(sample['Fedu'].values[0])
+    state.traveltime_idx = list(traveltime_options.values()).index(sample['traveltime'].values[0])
+    state.failures_idx = list(failures_options.values()).index(sample['failures'].values[0])
+    state.paid_idx = list(paid_options.values()).index(sample['paid'].values[0])
+    state.higher_idx = list(higher_options.values()).index(sample['higher'].values[0])
+    state.internet_idx = list(internet_options.values()).index(sample['internet'].values[0])
+    state.goout = sample['goout'].values[0]
+    state.G1 = sample['G1'].values[0]
+    state.G2 = sample['G2'].values[0]
+
+    if not set_value_only:
+        predict(sample,model_options[model_option], scaler_options[scaler_option])
 
 scaler_options ={"Standard Scaler":"standard.pkl", "Robust Scaler":"robust.pkl", "MinMax Scaler":"minmax.pkl"}
-model_options = {"Random Forest":"RandomForest.pkl", "SVM":"SVM.pkl", "Gradient Boosting":"gradient_boosting_model.pkl"}
+model_options = {"Random Forest":"RandomForest.pkl", "SVM":"SVM.pkl", "Gradient Boosting":"gradient_boosting.pkl"}
 sex_options = {"Female":"F", "Male": "M"}
 address_options = {"Urban":"U", "Rural": "R"}
 Medu_options = {"None":0, "Primary Education 4th grade": 1, "5th to 9th grade": 2, "Secondary Education": 3, "Higher Education": 4}
@@ -43,19 +64,21 @@ higher_options = {"Yes":"yes", "No":"no"}
 internet_options = {"Yes":"yes", "No":"no"}
 
 def main_render():
-    sex = st.sidebar.selectbox("Sex:", {"Female":"F", "Male": "M"})
-    age = st.sidebar.slider("Age", 6,  24)
-    address = st.sidebar.selectbox("Address:", {"Urban":"U", "Rural": "R"})
-    Medu = st.sidebar.selectbox("Mother's education level:", {"None":0, "Primary Education 4th grade": 1, "5th to 9th grade": 2, "Secondary Education": 3, "Higher Education": 4})
-    Fedu = st.sidebar.selectbox("Father's education level:", {"None":0, "Primary Education 4th grade": 1, "5th to 9th grade": 2, "Secondary Education": 3, "Higher Education": 4})
-    traveltime = st.sidebar.selectbox("Travel time to school:", {"<15 min":1, "15-30 min":2, "30-60 min":3, ">60 min":4})
-    failures = st.sidebar.selectbox("Number of past class failures:", {0:0, 1:1, 2:2, 3:3})
-    paid = st.sidebar.selectbox("Extra paid classes within the course subject:", {"Yes":"yes", "No":"no"})
-    higher = st.sidebar.selectbox("Wants to take higher education:", {"Yes":"yes", "No":"no"})
-    internet = st.sidebar.selectbox("Internet access at home:", {"Yes":"yes", "No":"no"})
-    goout = st.sidebar.slider("Going out with friends", 1, 5)
-    G1 = st.sidebar.number_input("G1")
-    G2 = st.sidebar.number_input("G2")
+    state = st.session_state;
+    st.sidebar.header("Parameters")
+    sex = st.sidebar.selectbox("Sex:", sex_options.keys(), 0 if 'sex_idx' not in state else state.sex_idx)
+    age = st.sidebar.slider("Age", 6,  24, 6 if 'age' not in state else state.age)
+    address = st.sidebar.selectbox("Address:", address_options.keys(), 0 if 'address_idx' not in state else state.address_idx)
+    Medu = st.sidebar.selectbox("Mother's education level:", Medu_options.keys(), 0 if 'Medu_idx' not in state else state.Medu_idx)
+    Fedu = st.sidebar.selectbox("Father's education level:", Fedu_options.keys(), 0 if 'Fedu_idx' not in state else state.Fedu_idx)
+    traveltime = st.sidebar.selectbox("Travel time to school:", traveltime_options.keys(), 0 if 'traveltime_idx' not in state else state.traveltime_idx)
+    failures = st.sidebar.selectbox("Number of past class failures:", failures_options.keys(), 0 if 'failures_idx' not in state else state.failures_idx)
+    paid = st.sidebar.selectbox("Extra paid classes within the course subject:", paid_options.keys(), 0 if 'paid_idx' not in state else state.paid_idx)
+    higher = st.sidebar.selectbox("Wants to take higher education:", higher_options.keys(), 0 if 'higher_idx' not in state else state.higher_idx)
+    internet = st.sidebar.selectbox("Internet access at home:", internet_options.keys(), 0 if 'internet_idx' not in state else state.internet_idx)
+    goout = st.sidebar.slider("Going out with friends", 1, 5, 1 if 'goout' not in state else state.goout)
+    G1 = st.sidebar.number_input("G1", 0 if 'G1' not in state else state.G1)
+    G2 = st.sidebar.number_input("G2", 0 if 'G2' not in state else state.G2)
     input_df = pd.DataFrame({
         'sex' : [sex_options[sex]],
         'age' : [age],
@@ -72,14 +95,59 @@ def main_render():
         'G2' : [G2]
     })
     st.sidebar.button('predict', on_click=lambda: predict(input_df,model_options[model_option], scaler_options[scaler_option]))
+    st.sidebar.button('predict on all models', on_click=lambda: pred_all_models(input_df, scaler_options[scaler_option]))
     st.sidebar.button('random predict', on_click=random_button_callback)
     if 'result' in st.session_state:
         st.write("prediction result: ",st.session_state.result)
+        
 
-def predict(input_df,model_option,scaler_option):
+def pred_all_models(input_df, scaler_option):
+    st.session_state.y_pred = []
+    result = {}
+    for model_option in model_options:
+        pred = predict(input_df, model_options[model_option], scaler_option)
+        st.session_state.y_pred.append(pred)
+        result[model_option] = pred
+    st.session_state.result = "\n".join([f"{k}: {v}" for k, v in result.items()])
+    show_plot()
+
+def show_plot():
+    # Membuat plot hasil prediksi
+    y_pred = st.session_state.y_pred
+    fig1, ax1 = plt.subplots()
+    ax1.bar(model_options.keys(), y_pred)
+    # ax1.xlabel("Model")
+    # ax1.ylabel("Prediction")
+    # ax1.title("Prediction Result")
+    st.pyplot(fig1)
+
+
+    # print(y_pred)
+    # fig1, ax1 = plt.subplots()
+    # # ax1.scatter(range(len(y)), y, color='blue', label='Data Aktual')
+    # ax1.scatter(range(len(y_pred)), y_pred, color='red', label='Prediksi', alpha=0.5)
+    # ax1.set_xlabel("Index")
+    # ax1.set_ylabel("Target Value")
+    # ax1.set_title("Prediction vs Actual")
+    # ax1.legend()
+    # st.pyplot(fig1)
+
+    # # Membuat residual plot
+    # residuals = y - y_pred
+    # fig2, ax2 = plt.subplots()
+    # ax2.scatter(y_pred, residuals, color='blue')
+    # ax2.axhline(y=0, color='r', linestyle='--')
+    # ax2.set_xlabel("Predicted")
+    # ax2.set_ylabel("Residuals")
+    # ax2.set_title("Residuals vs Predicted")
+    # st.pyplot(fig2)
+
+def predict(param_df,model_option,scaler_option):
+    input_df = param_df.copy()
     print(model_option, scaler_option)
     if 'model_option' not in st.session_state or st.session_state.model_option != model_option:
         try:
+            # st.session_state.model = joblib.load("Models/"+model_option)
             st.session_state.model = pickle.load(open("Models/"+model_option, 'rb'))
         except:
             st.session_state.model = joblib.load("Models/"+model_option)
@@ -91,7 +159,6 @@ def predict(input_df,model_option,scaler_option):
     encoder = LabelEncoder()
     original_df = pd.read_csv('student-mat.csv', sep=';', usecols=['sex', 'age', 'address', 'Medu', 'Fedu', 
      'traveltime', 'failures', 'paid', 'higher', 'internet','goout', 'G1', 'G2'])
-    # result = input_df.select_dtypes(include='number')
     input_df.address=encoder.fit(original_df.address).transform(input_df.address)
     input_df.sex=encoder.fit(original_df.sex).fit_transform(input_df.sex)
     input_df.paid=encoder.fit(original_df.paid).fit_transform(input_df.paid)
@@ -104,10 +171,15 @@ def predict(input_df,model_option,scaler_option):
     input_df = input_df.copy()
     input_df[result.columns] = scaled_df
     print(input_df)
+    print("model:",st.session_state.model)
     pred = st.session_state.model.predict(input_df)
     st.session_state.result = str(pred[0])
+    return pred[0]
         
 def main():
+    if 'initialized' not in st.session_state or not st.session_state.initialized:
+        st.session_state.initialized = True
+        random_button_callback(True)
     main_render()
     return 0
 
